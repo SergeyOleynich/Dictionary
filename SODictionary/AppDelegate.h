8df92d08ff548d9a985c57309ebbf0712d414890
Sergey <for_sale@I-hate-you-guys.local>
2015-06-27 12:47:09 +0300

    Initial Commit

---
+++ SODictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SODictionary
+//
+//  Created by Sergey on 27.06.15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SODictionary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SODictionary
+//
+//  Created by Sergey on 27.06.15.
+//  Copyright (c) 2015 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
